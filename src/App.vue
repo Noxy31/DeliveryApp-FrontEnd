0ec1f6f5e2bd18999a107d41c75dc6de73374b38
Noxy31 <nellenoar@gmail.com>
2024-10-23 23:08:42 +0200

    Initialized vue project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import './assets/base.css';
+</script>
+
+<template>
+
+
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style scoped>
+main {
+  padding-top: 10vh;
+  padding-left: 5vw;
+}
+</style>
